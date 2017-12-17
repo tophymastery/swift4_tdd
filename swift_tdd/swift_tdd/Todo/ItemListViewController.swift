babfa1f69615fca599b0d04bd9a7ad3be2d9d728
Akadet Mathurapoj <akadet.m@linecorp.com>
2017-12-17 22:27:21 +0700

    remove un-use initial ViewController

---
+++ swift_tdd/swift_tdd/Todo/ItemListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ItemListViewController.swift
+//  swift_tdd
+//
+//  Created by Akadet on 12/17/17.
+//  Copyright © 2017 Akadet. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView = UITableView()
+    } 
+
+}
